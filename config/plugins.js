f33da38eb152344c0e5b9a78f1aa0b1cc7c3d2de
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2024-09-26 10:09:33 -0400

    added gcp storage bucket for image uploads

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = {
+    upload: {
+        config: {
+            provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+            providerOptions: {
+                bucketName: 'rocsites-assets',
+                publicFiles: true,
+                uniform: false,
+                basePath: 'uploads-blue-rock',
+            },
+        },
+    },
+}
